caea2cb42b3e87a6a76a87a4ade9fbb47410b9f6
Baek <bch970802@gmail.com>
2021-04-25 16:37:02 +0900

    init

---
+++ src/main/java/com/baek/BaekFinalProject/BaekFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.baek.BaekFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baek/BaekFinalProject/BaekFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baek.BaekFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
